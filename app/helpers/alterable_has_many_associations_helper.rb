3386f82f27dfb9af9be29ffa81ae2ce117fb3b34
Elias Ojala <elias.jh.ojala@gmail.com>
2019-12-08 20:24:01 +0200

    Add helpers for alterable has many associations

---
+++ app/helpers/alterable_has_many_associations_helper.rb
@@ -0,0 +1,89 @@
+module AlterableHasManyAssociationsHelper
+  
+  def one_item(**options)
+    f = options[:f]
+    item_class = options[:item_class]
+    item_array_name = options[:item_array_name]
+    item = options[:item]
+    
+    item ||= item_class.new
+    item_for_fields = item.dup
+    item_for_fields.id = nil
+    f.fields_for item_array_name, item_for_fields do |sif|
+      tag.tr do
+        concat sif.hidden_field :id, value: item.id
+        item_fields = (options[:item_fields] || item_class::DEFAULT_FORM_FIELDS)
+        item_fields.each do |field|
+          concat tag.td(formFields(sif, item, field))
+        end
+      end
+    end
+  end
+  
+  def add_button
+    link_to(tag.span("add", class: "material-icons"), nil, class: 'add-item')
+  end
+  
+  def alterable_has_many_associations_form(**options)
+    
+    item_array_name = options[:item_array_name]
+    item_class = options[:item_class]
+    associated_object = options[:associated_object]
+    subjects = options[:subjects]
+    form_name = options[:form_name]
+    form_url = options[:form_url]
+    
+    form_for form_name, url: form_url, method: :post, html: { class: "general" } do |f|
+      options[:f] = f
+      options[:show_submit] = true
+      alterable_has_many_associations_form_contents(**options)
+    end
+    
+  end
+  
+  def alterable_has_many_associations_form_contents (**options)
+    
+    f = options[:f]
+    associated_object = options[:associated_object]
+    associated_object ||= options[:association]
+    associated_object ||= options[:assoc]
+    item_array_name = options[:item_array_name]
+    item_array_name ||= "#{associated_object.klass.name.pluralize.underscore}[]"
+    item_class = options[:item_class]
+    item_class ||= associated_object.klass
+    subjects = options[:subjects]
+    subjects ||= flat_fields(item_class::DEFAULT_FORM_FIELDS) rescue nil
+    form_name = options[:form_name]
+    form_url = options[:form_url]
+    item_fields = options[:item_fields]
+    disable_key_binds = "disable_key_binds" if options[:disable_key_binds]
+    directly_downward_with_enter = "directly_downward_with_enter" if options[:directly_downward_with_enter]
+    
+    tag.span(class: "alterable_has_many_associations_form #{disable_key_binds} #{directly_downward_with_enter}") do
+      general_table do
+        tag.tbody(class: "fields") do
+          tag.tr(class: "subjectRow") do
+            subjects.each do |subject, field|
+              concat tag.th(I18n.t("activerecord.attributes.#{item_class.name.underscore}.#{subject}"), class: subject)
+            end
+          end + capture do
+            associated_object.each do |item|
+              concat one_item(f: f, item: item, item_array_name: item_array_name, item_class: item_class, item_fields: item_fields)
+            end
+          end + capture do
+            concat one_item(f: f, item_array_name: item_array_name, item_class: item_class, item_fields: item_fields) unless associated_object.any?
+          end
+        end + capture do
+          concat tag.tr(tag.td(f.submit('Tallenna'), colspan: subjects.length)) if options[:show_submit]
+        end
+        
+      end + capture do
+        concat add_button unless options[:hide_add_button]
+        concat tag.template(one_item(f: f, item_array_name: item_array_name, item_class: item_class, item_fields: item_fields), id: "new-item-template") unless options[:hide_add_button]
+      end
+      
+    end
+    
+  end
+  
+end
---
+++ app/helpers/show_alterable_has_many_associations_helper.rb
@@ -0,0 +1,42 @@
+module ShowAlterableHasManyAssociationsHelper
+  
+  def show_one_item(**options)
+    item_class = options[:item_class]
+    item = options[:item]
+    fields = options[:fields]
+    fields ||= item_class::DEFAULT_FORM_FIELDS
+
+    tag.tr do
+      fields.each do |field|
+        concat tag.td(show_form_fields(item, field))
+      end
+    end
+  end
+
+  def show_alterable_has_many_associations_form_contents (**options)
+
+    item_class = options[:item_class]
+    associated_object = options[:associated_object]
+    subjects = options[:subjects]
+    form_name = options[:form_name]
+    form_url = options[:form_url]
+    
+    tag.span(class: "show_alterable_has_many_associations_form") do
+      general_table do
+        tag.tbody(class: "fields") do
+          tag.tr(class: "subjectRow") do
+            subjects.each do |subject, field|
+              concat tag.th(item_class.human_attribute_name(subject), class: subject)
+            end
+          end + capture do
+            associated_object.each do |item|
+              concat show_one_item(item: item, item_class: item_class, fields: options[:fields])
+            end
+          end
+        end
+      end
+    end
+    
+  end
+  
+end
