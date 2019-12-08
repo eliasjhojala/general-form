a4d0f35c53596b6dcaff64cd33152eff936b3aaf
Elias Ojala <elias.jh.ojala@gmail.com>
2019-12-08 16:43:48 +0200

    Update

--- app/controllers/concerns/fields.rb
+++ app/controllers/concerns/fields.rb
@@ -1,41 +1,61 @@
 module Fields
   
-  def flat_fields(fields)
+  def flat_fields(fields, **opts)
     fields = fields.values unless fields.class == Array
-    return fields.flatten.map {|x| [x.field_name, x]}.to_h
+    return_with_names = !opts.key?(:name) || opts[:name]
+    return_as_hash = return_with_names && (!opts.key?(:hash) || opts[:hash])
+    fields = fields.flatten.map do |x|
+      if x.is_a? Symbol
+        x = UsesFormField::FormField.new(field_name: x)
+      end
+      return_with_names ? [x.field_name, x] : x
+    end
+    return_as_hash ? fields.to_h : fields
   end
-  
+
   def field_names(fields)
-    fields = fields.values unless fields.class == Array
-    return fields.flatten.map(&:field_name)
+    flat_fields(fields).keys
   end
-  
+
   def permit_fields(fields)
-    fields = fields.values unless fields.class == Array
-    permitted_fields = flat_fields(fields).keys.push(:id)
-    fields.flatten.map {|x| if ([:collection_select, :flags_select].include? x.field_type) then permitted_fields.push({x.field_name => []}) end}
-    associated_fields(fields).each do |name, field|
-      field.associated_fields ||= field.associated_model::DEFAULT_FORM_FIELDS
-      permitted_fields.push("#{name}_attributes" => permit_fields(field.associated_fields))
+    permitted_fields = field_names(fields).push(:id)
+    associated_fields(fields)&.each do |name, field|
+      field = [field] unless field.class == Array
+      field.each { |f| f.associated_fields ||= f.associated_model::DEFAULT_FORM_FIELDS }
+      permitted_fields.push("#{name}_attributes" => permit_fields(field.map(&:associated_fields)))
     end
     files_fields(fields).each do |name, field|
       permitted_fields.push(name => [])
     end
+    multiple_select_fields(fields).each do |name, field|
+      permitted_fields.push(name => [])
+    end
+    collection_select_fields(fields).each do |name, field|
+      permitted_fields.push(name => [])
+    end
     return permitted_fields
   end
-  
+
   def associated_fields(fields)
-    fields = fields.values unless fields.class == Array
-    return fields.flatten.map {|x| if (x.field_type == :associated_fields) then [x.field_name.to_s, x] end}.compact.to_h
+    flat_fields(fields, hash: false).select do |k,v|
+      v.field_type == :associated_fields
+    end.group_by(&:first).map{|k,v| Hash[k, v.map(&:last)]}.inject(&:merge)
   end
-  
+
   def files_fields(fields)
-    fields = fields.values unless fields.class == Array
-    return fields.flatten.map {|x| if (x.field_type == :files) then [x.field_name.to_s, x] end}.compact.to_h
+    flat_fields(fields).select { |k,v| v.field_type == :files }
+  end
+
+  def multiple_select_fields(fields)
+    flat_fields(fields).select { |k,v| v.field_type == :select && v.multiple }
   end
   
+  def collection_select_fields(fields)
+    flat_fields(fields).select { |k,v| v.field_type == :collection_select }
+  end
+
   def associated_field_names(fields)
-    associated_fields(fields).keys.compact
+    associated_fields(fields).keys
   end
   
 end
