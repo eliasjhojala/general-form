8063539a5dcd1597eadc612ef064ad939336c4bb
Elias Ojala <elias.jh.ojala@gmail.com>
2025-03-31 17:33:10 +0300

    Return all changed items also when running callbacks on handle alterable has many association

--- app/controllers/concerns/alterable_has_many_association_handler.rb
+++ app/controllers/concerns/alterable_has_many_association_handler.rb
@@ -1,5 +1,5 @@
 module AlterableHasManyAssociationHandler
-  
+
   def handle_alterable_has_many_association **options
     association = options[:associated_object] || options[:association] || options[:assoc]
     is_real_association = (options.keys & [:associated_object, :association, :assoc]).any?
@@ -12,24 +12,24 @@ module AlterableHasManyAssociationHandler
     fields = options[:item_fields] || GeneralForm.default_fields[item_class]
     params_to_permit = (options[:params_to_permit] || permit_fields(fields)) | [:id]
     run_callbacks = options[:run_callbacks]
-    
+
     return if items.blank? || items[item_array_name].blank?
-    
+
     ActiveRecord::Base.transaction do
-    
+
       # Initialize arrays for inserts, updates and deletes
       items_to_save = []
       saved_items = []
       item_ids_to_delete = []
-        
+
       flat_fields_ = flat_fields(fields)
       habtm_field_names_ = habtm_field_names(fields)
       flags_select_field_names_ = flags_select_fields(fields).keys
       file_and_files_fields_names_ = file_and_files_fields(fields).keys
       check_box_fields_names_ = check_box_fields(fields).keys
-      
+
       prevent_activerecord_import = run_callbacks || habtm_field_names_.any? || flags_select_field_names_.any? || file_and_files_fields_names_.any?
-      
+
       if prevent_activerecord_import
         items_association_by_id = association.map { |item| [item.id, item] }.to_h
       else
@@ -38,7 +38,7 @@ module AlterableHasManyAssociationHandler
         items_relation = item_class.all.where(id: items[item_array_name].map{|obj|obj[:id]}.compact)
         items_relation_hash = items_relation.map { |obj| [obj.id, obj] }.to_h
       end
-      
+
       # Loop through all the items and either run commands on them or just initialize them for commands to be run later
       items[item_array_name].each do |item|
         item_id = item[id_field_name].present? ? item[id_field_name].to_i : nil
@@ -51,6 +51,7 @@ module AlterableHasManyAssociationHandler
               items_relation_hash[item_id]&.assign_attributes(permitted_params)
               items_to_save << items_relation_hash[item_id] if items_relation_hash[item_id]&.changed?
             else
+              items_to_save << items_association_by_id[item_id]
               items_association_by_id[item_id]&.update permitted_params
             end
           elsif options[:only_insert].blank? # If item should be deleted --> delete it
@@ -62,17 +63,15 @@ module AlterableHasManyAssociationHandler
             association << new_item
             saved_items << new_item
           else # If item shouldn't be associated to parent object
-            unless prevent_activerecord_import
-              items_to_save << new_item
-            else # If callbacks should be ran save item now
-              new_item.save
-            end
+            items_to_save << new_item
+            # If callbacks should be ran save item now
+            new_item.save if prevent_activerecord_import
           end
         end
       end # items.each
-      
+
       # Run all inserts, updates and deletes
-      if items_to_save.any?
+      if !prevent_activerecord_import && items_to_save.any?
         item_class.import params_to_permit, items_to_save, on_duplicate_key_update: :all
       end
       if item_ids_to_delete.any?
@@ -84,8 +83,8 @@ module AlterableHasManyAssociationHandler
       end
 
       items_to_save + saved_items
-      
+
     end # transaction
   end # method
-  
+
 end # module
